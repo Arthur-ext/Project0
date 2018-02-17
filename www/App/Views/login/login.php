bbbef654167446b3dd0e20dd434cad43b72f9267
Arthur <arthur.ext@gmail.com>
2018-02-17 00:32:52 -0200

    fix: login page (layout)

--- www/App/Views/login/login.php
+++ www/App/Views/login/login.php
@@ -1,30 +1,55 @@
 <?php $this->view->titlePage = "Entrar - Project0"; ?>
 
+<?php $this->view->links[] = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"; ?>
 <?php $this->view->links[] = "App/assets/login.css" ?>
-<?php $this->view->links[] = "App/assets/reset.css" ?>
-<?php $this->view->links[] = "App/assets/bootstrap/css/bootstrap.css" ?>
 
 <!-- header instance -->
 <?php include_once "App/Views/layouts/main-header.php"; ?>
 
-    <div class="container">
+    
+    <div class="container-screem-login">
         <div class="container-login">
-            <form action="/authLogin " method="POST">
+            <form id="form-login" action="/authLogin" method="POST">
                 <h5>ENTRAR</h5>
-                <input type="text" name="user" placeholder="Username" required >
-                <input type="password" name="pass" placeholder="Senha" required >
-                <!-- <input pattern="([0-9]{11})$" type="text"> -->
+                <div class="form-group">
+                    <label for="user">Nome de Usuário</label>
+                    <input class="form-control input-login" type="text" name="user" placeholder="Username" required >
+                </div>
+                <div class="form-group">
+                    <label for="pass">Senha</label>
+                    <input class="form-control input-login" type="password" name="pass" placeholder="Senha" required >
+                </div>
+                
+                <button class="btn btn-login" type="submit">ENTRAR</button>
+
+                <div class="notify-error">
+                    <span>
+                        <?= $_SESSION['errorLogin']; ?>
+                    </span>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    <?php // $this->addons('./script.js') ?>
+    <?php // $this->view->scripts[] = "./script" ?>
+
+<!-- footer instance -->
+<?php include_once "App/Views/layouts/main-footer.php" ?>
+
+
+
+
+            <!-- <form action="/authLogin " method="POST">
+                <h5>ENTRAR</h5>
+                
+                <input pattern="([0-9]{11})$" type="text">
 
 
                 <button type="submit" >Acessar</button>
                 <a href="/criar-conta">Cadastrar-se</a>
-            </form>
-
-            <?= $_SESSION['errorLogin']; ?>
-        </div>
-    </div>
-    
-    <?php // $this->addons('./script.js') ?>
-    <?php $this->view->scripts[] = "./script" ?>
-<!-- footer instance -->
-<?php include_once "App/Views/layouts/main-footer.php" ?>
+            </form> -->
